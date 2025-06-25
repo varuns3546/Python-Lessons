c1c29c1264f3f772c61a1a89e2bfd3c465005d22
Varun Seenivasan <varunseenivasan82786@gmail.com>
2025-06-25 13:46:06 -0500

    Create 03_intro_to_math.py

---
+++ Module 1 - Basics/03_intro_to_math.py
@@ -0,0 +1,43 @@
+# Introduction to Math
+
+"""
+The most common arithmetic operations you can perform are: 
+
+* Addition (+)
+* Subtraction (-)
+* Multiplication (*)
+* Division (/)
+* Integer Division (//): Divides the left operand by the right operand and removes the remainder, outputting a whole number
+* Modulus (%): Divides the left operand by the right operand and returns the remainder of the operation
+* Exponentiation (**) Raises the left operand by the power of the right operand
+"""
+
+# Addition
+addition = 2 + 3
+print("Addition:", addition)
+
+# Subtraction
+subtraction = 5 - 1
+print("Subtraction:", subtraction)
+
+# Multiplication
+multiplication = 4 * 2
+print("Multiplication:", multiplication)
+
+# Division
+division = 8 / 3
+print("Division:", division) 
+
+# Integer Division
+integer_division = 8 // 3
+print("Integer Division:", integer_division) 
+
+# Modulus
+modulus = 20 % 7
+print("Modulus:", modulus) 
+
+# Exponentiation
+exponentiation = 3 ** 3
+print("Exponentiation:", exponentiation) 
+
+# run the code and see the output
