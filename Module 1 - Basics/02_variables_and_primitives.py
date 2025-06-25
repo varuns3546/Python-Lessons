3f9d0ed3ab280f35c006b7e04fd124bccb93b994
Varun Seenivasan <varunseenivasan82786@gmail.com>
2025-06-25 13:45:46 -0500

    Create 02_variables_and_primitives.py

---
+++ Module 1 - Basics/02_variables_and_primitives.py
@@ -0,0 +1,32 @@
+# Variables and Primitives
+
+# Variables are used to store data
+# This is an example of a variable: 
+
+net_worth = -2
+
+# net_worth is the name of the variable
+# The '=', also known as the assignment operator, is used to assign net_worth a value of negative 2
+
+# Variables cannot have spaces and cannot begin with a number
+# Variables cannot have the same name as built-in Python commands
+
+# Primitives are the basic types of data built into python 
+# The four types of primitives are integers, floats, booleans and strings 
+
+# Integers, or ints, are any whole number
+
+int_example = 25
+
+# Floats are numbers that hold a decimal value
+
+float_example = 3.14
+
+# Booleans store values of True or False
+
+salad_is_good = False
+i_like_soup = True
+
+# Strings store text
+
+name = "Jimothy"
